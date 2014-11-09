0d93abf786ec1294886addad5f004be8e9b81763
kasumi <borovitina.ekaterina@gmail.com>
2014-11-09 22:33:14 +0500

    add help

--- main.cpp
+++ main.cpp
@@ -4,11 +4,28 @@
 #include "mazerecursion.hpp"
 #include "mazewave.hpp"
 
-int main()
-{
-  Maze *maze;
-  maze = new MazeRecursion();
-//  maze = new MazeWave();
+void help() {
+  std::cout << "Usage: [path_to_progarm] [mode]\n\n"
+            << "Available modes: \n"
+            << "0: wave with recursion\n"
+            << "1: wave with queue\n";
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    help();
+    return -1;
+  }
+
+  Maze* maze;
+  switch (std::atoi(argv[1])) {
+    default: case 0:
+      maze = new MazeRecursion();
+      break;
+    case 1:
+      maze = new MazeWave();
+      break;
+  }
 
   maze->Create(cv::Point2i(0, 0), true);
 
@@ -17,4 +34,3 @@ int main()
 
   return 0;
 }
-
